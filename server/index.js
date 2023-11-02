1e44d2f55d6b72632d8ac664bde6d8c4552e1b00
razamehdisyed <razamehdisyed@gmail.com>
2023-11-02 11:04:56 +0500

    setup

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import helmet from 'helmet'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 8800
+
+app.use(cors())
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.json({limit: "10mb"}))
+app.use(express.urlencoded({extended:true}))
+
+app.use(morgan("dev"))
+
+app.listen(PORT, () => {
+    console.log(`Dev server running on port: ${PORT}`)
+})

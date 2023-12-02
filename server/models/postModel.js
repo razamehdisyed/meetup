d05f892bc654bb6a5b16297c888ca80ab9951306
razamehdisyed <razamehdisyed@gmail.com>
2023-12-02 13:48:32 +0500

    a

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -8,7 +8,7 @@ const postSchema = new mongoose.Schema (
         description: { type: String, required: true},
         image: { type: String },
         likes: [{ type: String }],
-        Comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
+        comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
     },
         {timestamps: true}
 )

8b2d8c11f0f34faae3eca7ddcdc3bb90a6dcf40d
razamehdisyed <razamehdisyed@gmail.com>
2024-02-23 12:02:47 +0500

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { TbSocial } from 'react-icons/tb'
+import { BsShare } from 'react-icons/bs'
+import { ImConnection } from 'react-icons/im'
+import { AiOutlineInteraction } from 'react-icons/ai'
 import { useForm } from 'react-hook-form'
 import { Loading, CustomButton, TextInput } from '../components'
 import { Link } from 'react-router-dom'
@@ -12,11 +15,14 @@ const Login = () => {
   const [isSubmitting, setIsSubmitting] = useState(false)
   const dispatch = useDispatch()
 
+  const onSubmit = async(data) => {
+
+  }
   return (
     <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-6'>
       <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
         {/* LEFT */}
-        <div className='w-full lg:w=1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
+        <div className='w-full lg:w-1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
           <div className='w-full flex gap-2 items-center mb-6'>
             <div className='p-2 bg-[#065ad8] rounded text-white'>
               <TbSocial />
@@ -28,7 +34,8 @@ const Login = () => {
           <p className='text-ascent-1 text-base font-semibold'>Login to your account</p>
           <span className='text-sm mt-2 text-ascent-2'>Welcome back</span>
 
-          <form className='py-8 flex flex-col gap-5'>
+          <form className='py-8 flex flex-col gap-5'
+          onSubmit={handleSubmit(onSubmit)}>
             <TextInput
               name="email" placeholder="email@example.com"
               label="Email Address"
@@ -79,7 +86,7 @@ const Login = () => {
 
           <p className='text-ascent-2 text-sm text-center'>
             Dont't have an account?
-            <Link className='text-[#065ad8] font-semibold ml-2 cursor-pointer'>
+            <Link to='/register' className='text-[#065ad8] font-semibold ml-2 cursor-pointer'>
               Create Account
             </Link>
           </p>
@@ -93,7 +100,32 @@ const Login = () => {
             className='w-48 2xl:w-64 h-48 2xl:h-64 rounded-full object-cover'
 
             />
+            <div className='absolute flex items-center gap-1 bg-white
+            right-10 top-10 py-1 px-4 rounded-full'>
+              <BsShare size={14} />
+              <span className='text-xs font-medium'>Share</span>
+            </div>
+            <div className='absolute flex items-center gap-1 bg-white
+            left-6 top-5 py-1 px-4 rounded-full'>
+              <ImConnection />
+              <span className='text-xs font-medium'>Connect</span>
+            </div>
+            <div className='absolute flex items-center gap-1 bg-white
+            left-6 bottom-5 py-1 px-4 rounded-full'>
+              <AiOutlineInteraction />
+              <span className='text-xs font-medium'>Interact</span>
+            </div>
           </div>
+
+          <div className='mt-16 text-center'>
+            <p className='text-white text-base'>
+              Connect and share for fun
+            </p>
+            <span className='text-sm text-white/70'>
+              Share your special memories with friends 
+            </span>
+          </div>
+
         </div>
 
       </div>
